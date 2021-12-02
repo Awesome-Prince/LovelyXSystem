75fa03e91dd634413ed88855dea96b4a00a3a026
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-03 01:18:43 +0530

    Create safe.py

---
+++ Lovely_System/plugins/safe.py
@@ -0,0 +1,30 @@
+from Lovely_System import System, system_cmd
+import os
+import sys
+import subprocess
+
+
+@System.on(system_cmd(pattern=r"Lovely gitpull"))
+async def gitpull(event):
+    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+    await event.reply("Git pulled probably.")
+    os.system("restart.bat")
+    os.execv("start.bat", sys.argv)
+
+
+@System.on(system_cmd(pattern=r"Lovely restart"))
+async def reboot(event):
+    if event.fwd_from:
+        return
+    await event.reply("Restarting.....")
+    await System.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    sys.exit()
+
+
+@System.on(system_cmd(pattern=r"Lovely shutdown"))
+async def shutdown(event):
+    if event.fwd_from:
+        return
+    await event.reply("Shutting Down... ")
+    await System.disconnect()
