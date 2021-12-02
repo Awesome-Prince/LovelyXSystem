b7cd52ad00599ab0f8627987897f3e05f5b2d85f
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-03 00:52:16 +0530

    Create bancodes.py

---
+++ Lovely_System/plugins/bancodes.py
@@ -0,0 +1,24 @@
+help_plus = """ Here is Help for Bancodes  :-
+✯ • {LYx00} - Scammer 
+✯ • {LYx01} - SPAM ADDING MEMBER
+✯ • {LYx02} - CHILD ABUSE
+✯ • {LYx03} - ILLEGAL
+✯ • {LYx04} - FRAUD PROMOTION  [ANY KIND]
+✯ • {LYx05} - PHISHING
+✯ • {LYx06} - BAN EVASION
+✯ • {LYx07} - RAID/SPAM INFLAMED
+✯ • {LYx08} - ADDING SPAMBOTS
+✯ • {LYx09} - KRIMINALANT
+✯ • {LYx10} - SCAMMER
+✯ • {LYx11} - ABUSE SPAM
+✯ • {LYx12} - IMPERSONATION
+✯ • {LYx13} - MD/BTC SCAM
+✯ • {LYx14} - RAID INITIALIZER
+✯ • {LYx15} - RAID PARTICIPANT
+✯ • {LYx16} - SPAMBOT
+✯ • {LYx17} - CYBER THREATENING / CYBER BULLY
+✯ • {LYx18} - NSFW SPAMMER
+
+Powered By  Lovely Support
+"""
+__plugin_name__ = "BanCodes" 
