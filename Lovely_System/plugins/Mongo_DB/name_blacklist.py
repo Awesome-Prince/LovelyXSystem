1ec9578d547d3a080dc6f0517ff0ab09850b94ee
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-03 01:26:40 +0530

    Create name_blacklist.py

---
+++ Lovely_System/plugins/Mongo_DB/name_blacklist.py
@@ -0,0 +1,27 @@
+from Lovely_System import MONGO_CLIENT
+
+db = MONGO_CLIENT["Lovely"]["Main"]
+
+
+async def update_wlc_blacklist(word, add=False):
+    bl = await db.find_one({"_id": 2})
+    current = bl["blacklisted_wlc"]
+    upd, owo = {}, {}
+    if add:
+        if word in current:
+            return False
+        current.append(word)
+    else:
+        if word in current:
+            current.remove(word)
+        else:
+            return False
+    upd["blacklisted_wlc"] = current
+    owo["$set"] = upd
+    await db.update_one(await db.find_one({"_id": 2}), owo)
+    return True
+
+
+async def get_wlc_bl():
+    json = await db.find_one({"_id": 2})
+    return json.get("blacklisted_wlc", [])
