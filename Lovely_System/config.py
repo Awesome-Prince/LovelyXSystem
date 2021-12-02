3efdb649bd6188dc1c08a5cd60d0f5c228f98542
Lovely Boy 2 <92840649+lovelygf@users.noreply.github.com>
2021-12-03 00:27:21 +0530

    Create config.py

---
+++ Lovely_System/config.py
@@ -0,0 +1,13 @@
+from typing import Union, Optional
+
+API_ID: int = 12345
+API_HASH: str = "XYZ"
+STRING_SESSION: str = "XYZ:XYZ:XYZ...."
+Lovely_logs: Union[str, int] = -1001512548429
+Lovely_approved_logs: Union[str, int] = -1001476531091
+GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+BOT_TOKEN: str = "123456:shsiajskzkxhxj"
+ENFORCERS: int = 123488
+INSPECTORS: int = 1993883
+MONGO_DB_URL: str = "1993838:sjsh"
+# Creadits gose to https://github.com/Hodacka
